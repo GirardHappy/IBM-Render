c50199b655d676c790c9543144338501fe2d4757
GirardHappy <69600729+GirardHappy@users.noreply.github.com>
2023-02-24 19:16:29 +0100

    2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write("lol");
     res.end()
+  
 });
 
 server.listen(process.env.PORT || 5500);
